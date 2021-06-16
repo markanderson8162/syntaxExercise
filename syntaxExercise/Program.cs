121c8a9a79c893623ce4cafd39bcf676e72f83bd
Mark Anderson <markanderson8162@gmail.com>
2021-06-16 13:46:54 -0500

    shortened syntax

---
+++ syntaxExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace syntaxExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var answer = 4;
+			var response = "";
+
+			//answer < 9 ? response = answer + "is less than nine." : response = answer + "is greater than or equal to nine.";
+			Console.WriteLine(answer < 9 ? $"{answer} is less than nine." : $"{answer} is greater that or equal to.");
+		}
+	}
+}
